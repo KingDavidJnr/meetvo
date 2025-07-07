f21e6b6429fd3e402b1947d825a60383728b421e
David Oduse <odusedavid@gmail.com>
2025-07-07 18:00:10 +0100

    [chore] add db migration for description attribute in academic history table

--- models/academichistory.js
+++ models/academichistory.js
@@ -32,7 +32,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
       },
       description: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
       },
       start_year: {
         type: DataTypes.INTEGER,
